3f6591134130fb83289877863552f7f27ba06ae3
abhay0826R-dev <abhay0826r@gmail.com>
2026-05-14 10:20:54 +0530

    add new note function

--- src/components/Left.jsx
+++ src/components/Left.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import NoNoteSelected from './NoNoteSelected'
+import RightNote from './RightNote'
 
 const Left = () => {
   return (
-    <div className='w-4/5 h-full bg-gray-50 flex justify-center items-center'>
-      <NoNoteSelected />
+    // <div className='w-4/5 h-full bg-gray-50 flex justify-center items-center'>
+      // {/* <NoNoteSelected /> */}
+    // {/* </div> */}
+    <div className='w-4/5 h-full bg-gray-50 flex justify-center items-start my-20'>
+      <RightNote />
     </div>
   )
 }
---
+++ src/components/RightNote.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const RightNote = () => {
+
+    const setHeading = (e) => {
+        e.preventDefault()
+        console.log(e)
+    }
+
+  return (
+    <div>
+        <div className='mb-20 flex flex-col'>
+            <form onSubmit={setHeading}>
+                <input type='text' placeholder='Note Name' className='font-bold text-4xl border-b-2'></input>
+            </form>
+            <span>Last updated May 14, 2026 10:08 AM</span>
+        </div>
+        <article>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi vitae ab accusamus nemo perspiciatis aspernatur ratione! Odio sint magni necessitatibus?</p>
+        </article>
+    </div>
+  )
+}
+
+export default RightNote
